717a160fda3cb87c927cfddc61692a89d7148bcb
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-26 20:46:24 -0300

    Add new fields for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    sessions: 'users/sessions'
+  }
   resources :histories
 
   root "histories#index"
